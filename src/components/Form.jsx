89dade5cd74f999c5eb14e4ce82faaa842f74135
Kolten ✔️ <kolten.pilar@gmail.com>
2023-07-31 15:38:51 -0400

    updated styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,19 +15,23 @@ const Button = styled.input`
 color: #FFF;
 background-color: #232323;
 text-shadow: 0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18;
-height: 50px;
+width: 200px;
 font-size: 1.5rem;
 cursor: pointer;
 border-radius: 30px;
-&:hover { color: red;}
+&:hover { 
+  color: red;
+  box-shadow: 0 0 5px red;
+}
 `
 
 const myForm = {
   height: '50px',
   fontSize: '2.5rem',
   textAlign: 'center',
-  boxShadow: '0 0 5px #49ff18, 0 0 10px #49FF18, 0 0 15px #49FF18, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18',
+  boxShadow: '0 0 5px #49ff18, 0 0 5px #49FF18, 0 0 5px #49FF18, 0 0 10px #49ff18, 0 0 15px #49FF18, 0 0 20px #49FF18, 0 0 30px #49FF18, 0 0 30px #49ff18',
   borderRadius: '30px',
+
 }
 
 
@@ -53,8 +57,10 @@ function Form({ movieSearch }) {
   return (
     <div>
       <Container onSubmit={handleSubmit}>
+        <div style={{margin: 'auto', marginTop: 'auto', display: 'flex', flexDirection: 'row', }}>
           <input style={myForm} placeholder='Search for a Movie' type="text" value={form.searchTerm} onChange={handleChange} />
           <Button type="submit" value='Submit' /> 
+          </div>  
       </Container>
     </div>
   )
